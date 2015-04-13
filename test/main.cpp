4f8ba1710b91af03ca7246abc25141ec064254ec
Fabio Banfi <fbanfi90@gmail.com>
2015-04-13 14:37:07 +0200

    Changed .gitignore

--- test/main.cpp
+++ test/main.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    Life(256, 256).animate();
-    //Rule30(799, 400).animate();
+    //Life(256, 256).animate();
+    Rule30(799, 400).animate();
     //Rule30(899, 450, 0, true).animate();
 
     return 0;
